35425b95247c5760aaac0140469fc29a63db112c
Rutuja028 <rutujassr281@gmail.com>
2024-10-04 10:49:00 +0530

    changes

--- task/lib/homePage.dart
+++ task/lib/homePage.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,14 +14,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State {
   final TextEditingController _searchController = TextEditingController();
 
+  List<String> searchHistory = [];
   List data = [];
   bool showImg = false;
   int pageNumber = 0;
+
+  getHistory() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection("search_history").get();
+
+    List<QueryDocumentSnapshot> list = querySnapshot.docs;
+    log(list.toString());
+  }
+
   getData() async {
     http.Response response = await http.get(Uri.parse(
         'https://api.unsplash.com/photos/?client_id=IKPXpot9DDDV4ymCOHuxtGr6hUWcvXjYnIYWhcPNP2I&page=$pageNumber&per_page=10'));
 
     data = jsonDecode(response.body);
+    // log(data.toString());
+
     _assign();
     setState(() {
       showImg = true;
@@ -28,18 +42,21 @@ class _HomePageState extends State {
 
   _assign() {
     for (var i = 0; i < data.length; i++) {
-      images.add(data.elementAt(i)["urls"]["regular"]);
+      images.add(data.elementAt(i)["urls"]["small_s3"]);
+      // log(data.elementAt(i)["urls"]["regular"].toString());
       print("LENGTH:${images.length}");
     }
+    // log(images.toString());
   }
 
   List<String> images = [];
 
-  late ScrollController _scrollController = ScrollController();
+  late final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    getHistory();
     getData();
     _scrollController
       ..addListener(() {
@@ -56,7 +73,8 @@ class _HomePageState extends State {
   }
 
   Future<void> _searchImages(String query) async {
-    final String clientId = 'YOUR_UNSPLASH_ACCESS_KEY';
+    String clientId = 'IKPXpot9DDDV4ymCOHuxtGr6hUWcvXjYnIYWhcPNP2I';
+    log("In search images");
     final url =
         'https://api.unsplash.com/search/photos?page=$pageNumber&per_page=10&client_id=$clientId&query=$query';
 
@@ -69,7 +87,7 @@ class _HomePageState extends State {
       final Map<String, dynamic> result = jsonDecode(response.body);
       data = result['results'];
 
-      _assign(); // Same as your existing _assign method
+      _assign();
       setState(() {
         showImg = true;
       });
@@ -92,32 +110,47 @@ class _HomePageState extends State {
           children: [
             const Text("Welome to the Home Page!"),
             const SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  labelText: 'Search images...',
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: () {
-                      if (_searchController.text.isNotEmpty) {
-                        setState(() {
-                          pageNumber = 0;
-                          images
-                              .clear(); // Clear current images for new search results
-                        });
-                        _searchImages(_searchController.text);
-                      }
-                    },
-                  ),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            const SizedBox(
-                height: 20), // Add spacing between search bar and image list
+            DropdownMenu(
+              // initialSelection: menuItems.first,
 
+              trailingIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  if (_searchController.text.isNotEmpty) {
+                    pageNumber = 0;
+                    images
+                        .clear(); // Clear current images for new search results
+
+                    _searchImages(_searchController.text);
+                    searchHistory.add(_searchController.text);
+
+                    setState(() {});
+                  }
+                },
+              ),
+              controller: _searchController,
+              width: 300,
+              hintText: "Select Menu",
+              requestFocusOnTap: true,
+              enableFilter: true,
+              menuStyle: MenuStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlue.shade50),
+              ),
+
+              label: const Text('Select Menu'),
+              onSelected: (data) {},
+              enableSearch: true,
+
+              dropdownMenuEntries:
+                  searchHistory.map<DropdownMenuEntry>((String varHistory) {
+                return DropdownMenuEntry(
+                  value: varHistory,
+                  label: varHistory,
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 20),
             Container(
               height: 200,
               child: ListView.builder(
@@ -125,10 +158,11 @@ class _HomePageState extends State {
                 scrollDirection: Axis.horizontal,
                 itemCount: images.length,
                 itemBuilder: (context, index) {
+                  log("Images  ${images[0].toString()}");
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: !showImg
-                        ? CircularProgressIndicator()
+                        ? const CircularProgressIndicator()
                         : Image.network(images[index]),
                   );
                 },
